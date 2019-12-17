c9f00d10ba321c0b37c0e5aac9eb7c01b132e74c
maria liza <mariamartinez@pursuit.org>
2019-12-17 12:16:26 -0500

    work on css and try to fix ace buttons. will continue to work on it

--- 21Game.js
+++ 21Game.js
@@ -1,43 +1,40 @@
 document.addEventListener("DOMContentLoaded", () => {
   let startGameButton = document.querySelector("#startGame")
   let aceDiv = document.querySelector(".aceButtons")
-    let aceOne = document.querySelector("#aceOne");
-    let aceTwo = document.querySelector("#aceTwo");
- 
+  let aceOne = document.querySelector("#aceOne")
+  let aceTwo = document.querySelector("#aceTwo")
+
   const beginGame = async () => {
     try {
       let res = await axios.get(
         `https://deckofcardsapi.com/api/deck/new/draw/?count=2`
       )
 
-      if (res.data.cards[0].value !== "ACE") {
-        beginGame()
-        return
-      }
+      // if (res.data.cards[0].value !== "ACE") {
+      //   beginGame()
+      //   return
+      // }
 
       let playerScore = 0
       let dealerScore = 0
 
-
-
       const checkValue = cards => {
         cards.forEach(val => {
-          
           if (
-              val.value === "KING" ||
-              val.value === "QUEEN" ||
-              val.value === "JACK"
-              ) {
-            val.value = 10;
+            val.value === "KING" ||
+            val.value === "QUEEN" ||
+            val.value === "JACK"
+          ) {
+            val.value = 10
           } else if (val.value === "ACE") {
             // console.log(aceDiv)
             aceDiv.classList.remove("hidden")
             aceOne.onClick = function addToScore() {
-               val.value = 1;
+              val.value = 1
             }
             aceTwo.onClick = function addToScore() {
-                val.value = 11;
-             }
+              val.value = 11
+            }
           } else {
             val.value
           }
@@ -50,7 +47,7 @@ document.addEventListener("DOMContentLoaded", () => {
       cardPic1.src = res.data.cards[0].image
       p.appendChild(cardPic1)
 
-      let cardPic2 = document.createElement("img")
+      let cardPic2 = document.createElement("img")  
       cardPic2.src = res.data.cards[1].image
       p.appendChild(cardPic2)
 
