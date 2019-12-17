9e58d5e8725508717d35614590d24bd73e174e38
maria liza <mariamartinez@pursuit.org>
2019-12-17 02:44:10 -0500

    work on aces

--- 21Game.js
+++ 21Game.js
@@ -1,26 +1,45 @@
 document.addEventListener("DOMContentLoaded", () => {
   let startGameButton = document.querySelector("#startGame")
-
+  let aceDiv = document.querySelector(".aceButtons")
+    let aceOne = document.querySelector("#aceOne");
+    let aceTwo = document.querySelector("#aceTwo");
+ 
   const beginGame = async () => {
     try {
       let res = await axios.get(
         `https://deckofcardsapi.com/api/deck/new/draw/?count=2`
       )
+
+      if (res.data.cards[0].value !== "ACE") {
+        beginGame()
+        return
+      }
+
+      let playerScore = 0
+      let dealerScore = 0
+
+
+
       const checkValue = cards => {
-        let aces = []
-        cards.forEach(el => {
+        cards.forEach(val => {
+          
           if (
-            el.value === "KING" ||
-            el.value === "QUEEN" ||
-            el.value === "JACK"
-          ) {
-            el.value = 10
-          } else if (el.value === "ACE") {
-           
-            el.value = Math.floor(Math.random(aces.push(el.value)))
-            console.log(aces)
+              val.value === "KING" ||
+              val.value === "QUEEN" ||
+              val.value === "JACK"
+              ) {
+            val.value = 10;
+          } else if (val.value === "ACE") {
+            // console.log(aceDiv)
+            aceDiv.classList.remove("hidden")
+            aceOne.onClick = function addToScore() {
+               val.value = 1;
+            }
+            aceTwo.onClick = function addToScore() {
+                val.value = 11;
+             }
           } else {
-            el.value
+            val.value
           }
         })
       }
@@ -30,7 +49,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
       cardPic1.src = res.data.cards[0].image
       p.appendChild(cardPic1)
-      
+
       let cardPic2 = document.createElement("img")
       cardPic2.src = res.data.cards[1].image
       p.appendChild(cardPic2)
